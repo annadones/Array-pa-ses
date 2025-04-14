c6fc8488238a9ae575c8a85fed529ee2e6e0661f
annadones <gatonix.sirah@gmail.com>
2025-04-14 08:16:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+//Criar o vetor ARRAY
+
+var paises = ["Argentina", "Estados Unidos", "Canadá", "França", "Alemanha", "Brasil", "Japão", "China", "Austrália", "Itália", "México"]
+var paisesEscolhido = parseInt(prompt("Digite um número de 0 a 10"))
+
+//concatenar
+document.write ("países escolhido:"  + paises[paisesEscolhido])
